8d98c6c320f818bfaf34a3ae26717315371527ce
MQ\chu.thi.ngoc.huyen <chu.thi.ngoc.huyen@mqsolutions.com.vn>
2018-09-28 11:48:54 +0700

    no message

--- app/src/main/java/mq/com/chuohapps/AppConfigs.java
+++ app/src/main/java/mq/com/chuohapps/AppConfigs.java
@@ -17,6 +17,7 @@ public final class AppConfigs {
 
     public static final String HOST_MQ = "http://192.168.6.34:22222/api/";
     public static final String HOST = "http://192.168.47.103:22222/api/";
+    public static final String HOST_NAVIS = "http://navistardev.cloudapp.net:5005/api/";
     public static final int HTTP_TIMEOUT = 20;//in second
     public static boolean isEnableLog = true;
 
