5011abd09c8f446e0ae25957b2960a946727e491
fd-PC\Admin <huyenchu26>
2018-11-20 00:27:11 +0700

    no message

--- app/src/main/java/mq/com/chuohapps/ui/rfid/RFIDFragment.java
+++ app/src/main/java/mq/com/chuohapps/ui/rfid/RFIDFragment.java
@@ -2,6 +2,8 @@ package mq.com.chuohapps.ui.rfid;
 
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 import butterknife.BindView;
 import mq.com.chuohapps.R;
 import mq.com.chuohapps.customview.LoadMoreRecyclerView;
+import mq.com.chuohapps.customview.OnClickListener;
 import mq.com.chuohapps.data.helpers.network.response.Vehicle;
 import mq.com.chuohapps.ui.rfid.adapter.RFIDAdapter;
 import mq.com.chuohapps.ui.xbase.BaseFragment;
@@ -17,6 +20,12 @@ public class RFIDFragment extends BaseFragment<RFIDContract.Presenter> implement
 
     @BindView(R.id.listRFID)
     LoadMoreRecyclerView listRFID;
+    @BindView(R.id.imageBack)
+    ImageView imageBack;
+    @BindView(R.id.imageRight)
+    ImageView imageRight;
+    @BindView(R.id.textTitle)
+    TextView textTitle;
 
     List<Vehicle> vehicles = new ArrayList<>();
     RFIDAdapter adapter;
@@ -38,8 +47,21 @@ public class RFIDFragment extends BaseFragment<RFIDContract.Presenter> implement
 
     @Override
     protected void setupViews(@NonNull View view) {
+        imageBack.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onDelayedClick(View v) {
+                moveBack();
+            }
+        });
+        imageRight.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onDelayedClick(View v) {
+                // TODO: 11/20/2018 search
+            }
+        });
+        textTitle.setText("RFID");
         adapter = new RFIDAdapter();
-        listRFID.setAdapter(adapter);
+//        listRFID.setAdapter(adapter);
     }
 
     @Override
